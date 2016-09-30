1e8552c579898b36bf4f84515b7892fef57fc7cd
TIMOTHY RIDGE SIMKINS <trs22@psu.edu>
2016-09-30 11:05:18 -0400

    Added homepage image.

---
+++ agsci/atlas/theme/theme/js/atlas.js
@@ -0,0 +1,17 @@
+$(document).ready(
+    function () {
+        
+        var hpi = $('.homepageimage');
+        
+        var image_urls = hpi.attr('data-image-urls').split(' ');
+        var image_heights = hpi.attr('data-image-heights').split(' ');
+
+        var randomnumber = Math.floor(Math.random()*image_urls.length);
+        var _height = image_heights[randomnumber];
+        var _url = image_urls[randomnumber];
+        
+        hpi.css('background-image', "url(" + _url + ")");
+        hpi.css('height', _height + 'px');
+        
+    }
+);
