bd48a353791e0a98e32d23f6ce63e885292f5447
TIMOTHY RIDGE SIMKINS <trs22@psu.edu>
2016-09-30 11:10:43 -0400

    Splitting on ';' instead of ' '

--- agsci/atlas/theme/theme/js/atlas.js
+++ agsci/atlas/theme/theme/js/atlas.js
@@ -3,8 +3,8 @@ $(document).ready(
         
         var hpi = $('.homepageimage');
         
-        var image_urls = hpi.attr('data-image-urls').split(' ');
-        var image_heights = hpi.attr('data-image-heights').split(' ');
+        var image_urls = hpi.attr('data-image-urls').split(';');
+        var image_heights = hpi.attr('data-image-heights').split(';');
 
         var randomnumber = Math.floor(Math.random()*image_urls.length);
         var _height = image_heights[randomnumber];
