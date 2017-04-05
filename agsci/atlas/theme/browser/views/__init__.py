372b52974bbfcdb7a7fb2ba83b1293bab84fe064
TIMOTHY RIDGE SIMKINS <trs22@psu.edu>
2017-04-05 14:57:34 -0400

    Added a more sophisticated "VirtualBrain" that really emulates a catalog brain. CSS updates

--- agsci/atlas/theme/browser/views/__init__.py
+++ agsci/atlas/theme/browser/views/__init__.py
@@ -2,21 +2,47 @@ from DateTime import DateTime
 from plone.memoize import ram
 from time import time
 
+import base64
 import json
 import logging
+import pickle
 import urllib2
 
+from zope.component.hooks import getSite
+from Products.CMFCore.utils import getToolByName
+from Products.ZCatalog.CatalogBrains import AbstractCatalogBrain
+
 from Products.agCommon.browser.views import FolderView
+from collective.contentleadimage.config import IMAGE_FIELD_NAME
 
-vpbf_timeout = 600
+VBF_TIMEOUT = 600
 
-def vpbf_timeout_cachekey(method, user_id):
-    return (method, user_id, time() // vpbf_timeout)
+def vbf_timeout_cachekey(method, user_id):
+    return (method, user_id, time() // VBF_TIMEOUT)
 
-@ram.cache(vpbf_timeout_cachekey)
-def VirtualPersonBrainFactory(user_id):
+class leadimage_field(object):
 
-    url = 'http://cms.extension.psu.edu/directory/%s/@@api/json?bin=False' % user_id
+    def __init__(self, url, alt=''):
+        self.url = url
+        self.alt = alt
+
+    def get_size(self):
+        return True
+
+    def get(self, context):
+        return self
+
+    def tag(self, *args, **kwargs):
+        v = u"""
+                    <img class="%s" src="%s/@@images/leadimage/leadimage" alt="%s" />
+                """.strip()
+        return v % (kwargs.get('css_class', ''), self.url, self.alt)
+
+@ram.cache(vbf_timeout_cachekey)
+def VirtualBrainFactory(user_id):
+
+    url = 'http://cms.extension.psu.edu/directory/%s/@@api/json?bin=False&brain=True' % user_id
+    url = 'http://agplonesrv.ag.psu.edu/atlas-test/directory/%s/@@api/json?bin=False&brain=True' % user_id
 
     try:
         data = json.loads(urllib2.urlopen(url).read())
@@ -27,88 +53,75 @@ def VirtualPersonBrainFactory(user_id):
 
     logger.info("Lookup %s, found %r" % (user_id, not not data))
 
-    return VirtualBrain(data)
+    class VirtualBrain(AbstractCatalogBrain):
 
+        def getURL(self):
+            return self.data.get('plone_url', '')
 
-class VirtualBrain(object):
+        def getObject(self):
+            return self
 
-    def __init__(self, json={}):
-        self.json = json
+        @property
+        def Type(self):
+            return self.data.get('product_type', '')
 
-        for (k,v) in json.iteritems():
-            setattr(self, k, v)
+        def pretty_title_or_id(self):
+            return self.Title
 
-    def CreationDate(self):
-        if hasattr(self, 'updated_at'):
-            return DateTime(self.updated_at)
-        return DateTime()
+        @property
+        def portal_type(self):
+            return 'FSDPerson'
 
-    def Creator(self):
-        return ''
+        def zip_code(self):
+            return self.data.get('zip', '')
 
-    def ModificationDate(self):
-        return self.CreationDate()
+        def getJobTitles(self):
+            return self.data.get('person_job_titles', [])
 
-    def EffectiveDate(self):
-        return self.CreationDate()
+        def getEmail(self):
+            return self.data.get('email_address', '')
 
-    @property
-    def Title(self):
-        return self.name
+        def getOfficePhone(self):
+            return self.data.get('phone', '')
 
-    @property
-    def Description(self):
-        return u''
+        def getField(self, field_name):
 
-    def getURL(self):
-        return getattr(self, 'plone_url', '')
+            if field_name in ['image', IMAGE_FIELD_NAME]:
+                return leadimage_field(self.getURL(), self.Title)
 
-    @property
-    def Type(self):
-        return getattr(self, 'product_type', '')
+        def hasContentLeadImage(self):
+            return not not self.data.get(u'has_lead_image', False)
 
-    def absolute_url(self):
-        return self.getURL()
+        def sortKey(self):
+            return tuple([
+                self.data.get(x, '') for x in ['last_name', 'first_name', 'middle_name']
+            ])
 
-    def getId(self):
-        return getattr(self, 'short_name', '')
+    # Setting record schema.  Required for brain, and lifted directly from
+    # Products/ZCatalog/Catalog.py
+    site = getSite()
+    portal_catalog = getToolByName(site, 'portal_catalog')
 
-    def getObject(self):
-        return self
+    catalog = portal_catalog._catalog
+    scopy = catalog.schema.copy()
 
-    @property
-    def hasContentLeadImage(self):
-        return False
+    schema_len = len(catalog.schema.keys())
 
-    def has_lead_image(self):
-        return False
+    scopy['data_record_id_'] = schema_len
+    scopy['data_record_score_'] = schema_len + 1
+    scopy['data_record_normalized_score_'] = schema_len + 2
 
-    @property
-    def portal_type(self):
-        return 'FSDPerson'
+    VirtualBrain.__record_schema__ = scopy
 
-    def pretty_title_or_id(self):
-        return getattr(self, 'name', '')
+    VirtualBrain.data = dict(data)
 
-    def review_state(self):
-        return getattr(self, 'plone_status', '')
+    brain_data = data.get('brain', None)
 
-    def zip_code(self):
-        return getattr(self, 'zip', '')
+    if brain_data:
+        brain_data = pickle.loads(brain_data)
+        return VirtualBrain(brain_data)
 
-    def getJobTitles(self):
-        return getattr(self, 'person_job_titles', [])
-
-    def getEmail(self):
-        return getattr(self, 'email_address', '')
-
-    def getOfficePhone(self):
-        return getattr(self, 'phone', '')
-
-    def sortKey(self):
-        return tuple([
-            getattr(self, x, '') for x in ['last_name', 'first_name', 'middle_name']
-        ])
+    return None
 
 
 class PeopleCollection(FolderView):
@@ -133,7 +146,10 @@ class PeopleCollection(FolderView):
                 if people_ids:
 
                     # Get a list of Virtual Brains
-                    v = [VirtualPersonBrainFactory(x) for x in people_ids]
+                    v = [VirtualBrainFactory(x) for x in people_ids]
+
+                    # Strip out non-existent brains
+                    v = [x for x in v if x]
 
                     # Sort by Sort Key
                     v.sort(key=lambda x: x.sortKey())
