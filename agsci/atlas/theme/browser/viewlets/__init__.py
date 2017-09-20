57e900585054699f7a5f918d51ddc9a901a4fdcb
TIMOTHY RIDGE SIMKINS <trs22@psu.edu>
2017-09-20 16:24:29 -0400

    * Split the "get people info from CMS" into its own method in .utilities, returning virtual brains  * Fixed the people collection to use that method  * Created a contributors viewlet that overrides the agCommon one, and uses that method.  * Updated rules and CSS to show in public view.

--- agsci/atlas/theme/browser/viewlets/__init__.py
+++ agsci/atlas/theme/browser/viewlets/__init__.py
@@ -1,7 +1,10 @@
 from zope.viewlet.interfaces import IViewletManager
 from Products.Five.viewlet.manager import ViewletManagerBase
 from Products.agCommon.browser.viewlets import AgCommonViewlet
+from Products.agCommon.browser.viewlets import ContributorsViewlet as _ContributorsViewlet
 from plone.app.layout.viewlets.common import PathBarViewlet as _PathBarViewlet
+
+from agsci.atlas.theme.utilities import getPeopleBrains
 
 try:
     from zope.app.component.hooks import getSite
@@ -15,13 +18,31 @@ class AtlasViewletManager(ViewletManagerBase):
 class IAtlasViewletManager(IViewletManager):
     # Viewlet manager interface
     pass
-    
+
 class AtlasViewlet(AgCommonViewlet):
     pass
-    
+
 class PathBarViewlet(_PathBarViewlet, AgCommonViewlet):
-    
-    @property 
+
+    @property
     def navigation_root_url(self):
         return self.portal_state.navigation_root_url()
+
+class ContributorsViewlet(_ContributorsViewlet):
+
+    def getPeopleResults(self, peopleList):
+        return getPeopleBrains(peopleList)
+
+    def getPersonInfo(self, brain):
+        job_titles = brain.getJobTitles()
+
+        return {
+            'name' : brain.Title,
+            'title' : job_titles and job_titles[0] or '',
+            'url' : brain.getURL(),
+            'phone' : brain.getOfficePhone(),
+            'email' : brain.getEmail(),
+            'image' : brain.getField('image'),
+            'tag' : brain.getField('image').tag
+        }
 
--- agsci/atlas/theme/browser/views/__init__.py
+++ agsci/atlas/theme/browser/views/__init__.py
@@ -1,137 +1,5 @@
-from DateTime import DateTime
-from plone.memoize import ram
-from time import time
-
-import base64
-import json
-import logging
-import pickle
-import urllib2
-
-from zope.component.hooks import getSite
-from Products.CMFCore.utils import getToolByName
-from Products.ZCatalog.CatalogBrains import AbstractCatalogBrain
-
 from Products.agCommon.browser.views import FolderView
-from collective.contentleadimage.config import IMAGE_FIELD_NAME
-
-VBF_TIMEOUT = 600
-
-def vbf_timeout_cachekey(method, user_id):
-    return (method, user_id, time() // VBF_TIMEOUT)
-
-class leadimage_field(object):
-
-    def __init__(self, url, alt=''):
-        self.url = url
-        self.alt = alt
-
-    def get_size(self):
-        return True
-
-    def get(self, context):
-        return self
-
-    def tag(self, *args, **kwargs):
-        v = u"""
-                    <img class="%s" src="%s/@@images/leadimage/leadimage" alt="%s" />
-                """.strip()
-        return v % (kwargs.get('css_class', ''), self.url, self.alt)
-
-@ram.cache(vbf_timeout_cachekey)
-def VirtualBrainFactory(user_id):
-
-    url = 'http://cms.extension.psu.edu/directory/%s/@@api/json?bin=False&brain=True' % user_id
-
-    try:
-        data = json.loads(urllib2.urlopen(url).read())
-    except urllib2.HTTPError:
-        data = {}
-
-    # Skip inactive (not "published") people.
-    if not data.get('visibility', None) in ('Catalog',):
-        return None
-
-    logger = logging.getLogger('agsci.atlas.theme')
-
-    logger.info("Lookup %s, found %r" % (user_id, not not data))
-
-    class VirtualBrain(AbstractCatalogBrain):
-
-        def getPloneURL(self):
-            return self.data.get('plone_url', '')
-
-        def getURL(self):
-            url = self.data.get('magento_url', '')
-
-            if url:
-                return u'https://extension.psu.edu/%s' % url
-
-        def getObject(self):
-            return self
-
-        @property
-        def Type(self):
-            return self.data.get('product_type', '')
-
-        def pretty_title_or_id(self):
-            return self.Title
-
-        @property
-        def portal_type(self):
-            return 'FSDPerson'
-
-        def zip_code(self):
-            return self.data.get('zip', '')
-
-        def getJobTitles(self):
-            return self.data.get('person_job_titles', [])
-
-        def getEmail(self):
-            return self.data.get('email_address', '')
-
-        def getOfficePhone(self):
-            return self.data.get('phone', '')
-
-        def getField(self, field_name):
-
-            if field_name in ['image', IMAGE_FIELD_NAME]:
-                return leadimage_field(self.getPloneURL(), self.Title)
-
-        def hasContentLeadImage(self):
-            return not not self.data.get(u'has_lead_image', False)
-
-        def sortKey(self):
-            return tuple([
-                self.data.get(x, '') for x in ['last_name', 'first_name', 'middle_name']
-            ])
-
-    # Setting record schema.  Required for brain, and lifted directly from
-    # Products/ZCatalog/Catalog.py
-    site = getSite()
-    portal_catalog = getToolByName(site, 'portal_catalog')
-
-    catalog = portal_catalog._catalog
-    scopy = catalog.schema.copy()
-
-    schema_len = len(catalog.schema.keys())
-
-    scopy['data_record_id_'] = schema_len
-    scopy['data_record_score_'] = schema_len + 1
-    scopy['data_record_normalized_score_'] = schema_len + 2
-
-    VirtualBrain.__record_schema__ = scopy
-
-    VirtualBrain.data = dict(data)
-
-    brain_data = data.get('brain', None)
-
-    if brain_data:
-        brain_data = pickle.loads(brain_data)
-        return VirtualBrain(brain_data)
-
-    return None
-
+from agsci.atlas.theme.utilities import getPeopleBrains
 
 class PeopleCollection(FolderView):
 
@@ -154,8 +22,8 @@ class PeopleCollection(FolderView):
                 # If we have people_ids:
                 if people_ids:
 
-                    # Get a list of Virtual Brains
-                    v = [VirtualBrainFactory(x) for x in people_ids]
+                    # Get a list of Virtual Brains of people
+                    v = getPeopleBrains(people_ids)
 
                     # Strip out non-existent brains
                     v = [x for x in v if x]
